5ffcc36514c7aebbc0c7bd0672f71ffccd554cc0
sharkawy <ahmedelsharkawy7>
2025-11-03 21:53:57 +0200

    edit routes and remove button bg/green

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from './Components/Layout/Layout.jsx';
-import Dashboard from './Components/Dashboard/Dashboard.jsx';
-import Subjects from './Components/Subjects/Subjects.jsx';
-import Announcements from './Components/Announcements/Announcements.jsx';
-import Schedule from './Components/Schedule/Schedule.jsx';
-import Faculty from './Components/Faculty/Faculty.jsx';
-import Login from './Components/Login/Login.jsx';
-import Register from './Components/Register/Register.jsx';
-import Profile from './Components/Profile/Profile.jsx';
-import NotFound from './Components/NotFound/NotFound.jsx';
-import AuthContextProvider from './Contexts/AuthContextProvider.jsx';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Components/Layout/Layout.jsx";
+import Dashboard from "./Components/Dashboard/Dashboard.jsx";
+import Subjects from "./Components/Subjects/Subjects.jsx";
+import Announcements from "./Components/Announcements/Announcements.jsx";
+import Schedule from "./Components/Schedule/Schedule.jsx";
+import Faculty from "./Components/Faculty/Faculty.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Register from "./pages/Register/Register.jsx";
+import Profile from "./Components/Profile/Profile.jsx";
+import NotFound from "./Components/NotFound/NotFound.jsx";
+import AuthContextProvider from "./Contexts/AuthContextProvider.jsx";
 
 const routers = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     children: [
-      { index: true, element: <Register /> },
-      { path: 'register', element: <Register /> },
-      { path: 'login', element: <Login /> },
-      { path: 'dashboard', element: <Dashboard /> },
-      { path: 'subjects', element: <Subjects /> },
-      { path: 'schedule', element: <Schedule /> },
-      { path: 'announcements', element: <Announcements /> },
-      { path: 'faculty', element: <Faculty /> },
-      { path: 'profile', element: <Profile /> },
-      { path: '*', element: <NotFound /> },
+      // { index: true, element: <Register /> },
+      // { path: 'register', element: <Register /> },
+      // { path: 'login', element: <Login /> },
+      { path: "dashboard", element: <Dashboard /> },
+      { path: "subjects", element: <Subjects /> },
+      { path: "schedule", element: <Schedule /> },
+      { path: "announcements", element: <Announcements /> },
+      { path: "faculty", element: <Faculty /> },
+      { path: "profile", element: <Profile /> },
+      { path: "*", element: <NotFound /> },
     ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 
